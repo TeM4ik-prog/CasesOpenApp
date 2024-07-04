a773fe8149674a66f6dc443a820384f8450d5bdb
TeM4ik-prog <167311099+TeM4ik-prog@users.noreply.github.com>
2024-07-04 12:15:00 +0300

    new commit

--- client/src/components/fieldOpenCases/fieldOpenCases.jsx
+++ client/src/components/fieldOpenCases/fieldOpenCases.jsx
@@ -16,28 +16,42 @@ export default function FieldOpenCases() {
     const [boxLoot, setBoxLoot] = useState([])
     const [isBoxOpening, setIsBoxOpening] = useState(false)
 
+    const [isAllImagesLoaded, setIsAllImagesLoaded] = useState(false);
+
+    const loadImage = async (url) => {
+        console.log('img Loaded')
+        console.log(url)
+        return new Promise((resolve) => {
+            const img = new Image();
+            img.onload = () => resolve(url);
+            img.src = url;
+
+
+        });
+    };
+
 
 
     let onOpen = (e) => {
+
         e.preventDefault()
-
-       
         setIsBoxOpening(true)
-        // alert('open ' + moneyToOpen)
-
 
         axios.post(
             `${localSitePath}/private/open`,
             { moneyToOpen })
-            .then((response) => {
+            .then(async (response) => {
                 console.log("User data", response.data)
 
                 handleTriggerUpdateUser()
                 setBoxLoot(response.data.Result_Loot_box)
 
+                await Promise.all(response.data.Result_Loot_box.map(image => loadImage(image.img)));
+                setIsAllImagesLoaded(true)
+
                 setTimeout(() => {
                     onScrollIntoView()
-                }, 100);
+                }, 300);
 
             })
             .catch((error) => {
@@ -49,21 +63,26 @@ export default function FieldOpenCases() {
         const elementPos = element.getBoundingClientRect();
         const containerPos = container.getBoundingClientRect();
 
+        const lootBoxContainer = document.querySelector('.loot-box-container');
+
         const containerCenter = containerPos.width / 2;
         const elementCenter = elementPos.left + (elementPos.width / 2);
 
-        const scrollPos = containerCenter - elementCenter;
+        console.log(lootBoxContainer.getBoundingClientRect())
+        const scrollPos = containerCenter - elementCenter + containerPos.width
 
-        const lootBoxContainer = document.querySelector('.loot-box-container');
+
         lootBoxContainer.style.transition = `left ${duration}ms cubic-bezier(0.075, 0.82, 0.165, 1)`;
         lootBoxContainer.style.left = `${scrollPos}px`;
 
 
         setTimeout(() => {
-            lootBoxContainer.style.left = `0px`;
             lootBoxContainer.style.transition = `0s`;
+            lootBoxContainer.style.left = `100%`;
+            
             setBoxLoot([])
             setIsBoxOpening(false)
+            setIsAllImagesLoaded(false)
         }, duration);
 
 
@@ -73,7 +92,6 @@ export default function FieldOpenCases() {
     let onScrollIntoView = () => {
         const elem = document.getElementById('loot_19');
         const container = document.querySelector('.openCase');
-        console.log(elem.getBoundingClientRect())
 
         scrollToElementX(container, elem, 8000);
     }
@@ -81,25 +99,26 @@ export default function FieldOpenCases() {
     return (
 
         <div className="fieldOpen-container">
+
             <div className="openCase">
-                <div className="line"></div>
+                <div className="line" />
 
-                {/* <ItemsList array_items={boxLoot} /> */}
+                {isBoxOpening && (
+                    <>
+                        {!isAllImagesLoaded && <p className="text-loading-items"
+                        >Загрузка предметов...</p>}
+                    </>
+                )}
+
                 <div className="loot-box-container">
-
-
-
                     {boxLoot.map((loot, index) => (
-
                         <OneItem key={index} id={index} item_info={loot} />
-
                     ))}
-
-
                 </div>
             </div>
 
-            <form onSubmit={!isBoxOpening ? onOpen : null} className="butts-container">
+
+            <form onSubmit={!isBoxOpening ? onOpen : (e) => e.preventDefault()} className="butts-container">
 
                 <button className={`OpenButt ${!isBoxOpening ? 'active' : null}`}>
                     Открыть({moneyToOpen ? moneyToOpen : 'не указанно'})
@@ -111,7 +130,9 @@ export default function FieldOpenCases() {
                     onChange={(e) => (setMoneyToOpen(e.target.value))}
                 />
             </form>
-        </div>
+
+
+        </div >
 
 
     )
